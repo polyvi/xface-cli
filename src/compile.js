40537cf2e249a1c82f0669381ddb766397a84615
chenxy <chenxy@polyvi.com>
2014-04-25 16:52:38 +0800

    在编译ios工程的时候根据据插件中静态库的architecture来设置设置工程编译时所使用的archs

--- src/compile.js
+++ src/compile.js
@@ -21,6 +21,10 @@
 /*jslint sloppy: true, white: true, newcap: true */
 
 var path              = require('path'),
+    fs                = require('fs'),
+    glob              = require('glob'),
+    _                 = require('underscore'),
+    shell             = require('shelljs'),
     cordova_util      = require('./util'),
     hooker            = require('./hooker'),
     superspawn        = require('./superspawn');
@@ -35,12 +39,30 @@ module.exports = function compile(options) {
     options.platforms.forEach(function(platform) {
         ret = ret.then(function() {
             var args = options.options.slice(0); // clone
+
+            var buildBackupFile;
             // default just build device package for ios
-            if (platform == 'ios' && args.indexOf('--device') < 0 && args.indexOf('--emulator') < 0) {
-                args.push('--device');
+            if (platform === 'ios') {
+                if(args.indexOf('--device') < 0 && args.indexOf('--emulator') < 0) {
+                    args.push('--device');
+                }
+                var archs = collectIosPluginLibraryArchs(projectRoot);
+                if(needModifyArchs(archs)) {
+                    var originalFile = path.join(projectRoot, 'platforms', platform, 'cordova', 'build'),
+                        buildBackupFile = path.join(projectRoot, 'platforms', platform, 'cordova', 'build-backup-for-project');
+                    !fs.existsSync(buildBackupFile) && shell.cp('-f', originalFile, buildBackupFile);
+                    shell.chmod('a+x', buildBackupFile);
+                    modifyBuildArchitecture(originalFile, archs);
+                }
             }
             var cmd = path.join(projectRoot, 'platforms', platform, 'cordova', 'build');
-            return superspawn.spawn(cmd, args, { stdio: 'inherit', printCommand: true });
+            return superspawn.spawn(cmd, args, { stdio: 'inherit', printCommand: true })
+            .then(function(data) {
+                if(platform === 'ios' && fs.existsSync(buildBackupFile)) {
+                    shell.mv('-f', buildBackupFile, cmd);
+                }
+                return data;
+            });
         });
     });
     ret = ret.then(function() {
@@ -48,3 +70,48 @@ module.exports = function compile(options) {
     });
     return ret;
 };
+
+function needModifyArchs(archs) {
+    return archs != 'default';
+}
+
+/**
+ * 收集所有插件静态库均支持的architecture交集
+ * 可能的值包含armv7, armv7s, arm64, default
+ */
+function collectIosPluginLibraryArchs(projectRoot) {
+    var parser = new (require('./metadata/ios_parser'))(path.join(projectRoot, 'platforms', 'ios'));
+    var pluginsDir = path.join(parser.cordovaproj, 'Plugins');
+    if(!fs.existsSync(pluginsDir)) return 'default';
+    var staticLibs = glob.sync(path.join(pluginsDir, '**', '*.a')),
+        frameworks = glob.sync(path.join(pluginsDir, '**', '*.framework'));
+    frameworks.forEach(function(framework) {
+        var name = path.basename(framework, path.extname(framework));
+        var libPath = path.join(framework, name);
+        fs.existsSync(libPath) && staticLibs.push(libPath);
+    });
+    if(staticLibs.length <= 0) return 'default';
+
+    var archs = ['armv7', 'armv7s', 'arm64'],
+        expr = /.*:.*:\s*(.*?)\s*$/i;
+    staticLibs.forEach(function(lib) {
+        var output = shell.exec('lipo -info "' + lib + '"', {silent:true}).output;
+        var matchData = output.match(expr);
+        if(matchData) {
+            archs = _.intersection(archs, matchData[1].split(/\s+/));
+        }
+    });
+    return archs;
+}
+
+function modifyBuildArchitecture(buildPath, archs) {
+    var grossExpr = /^.*-sdk\s+iphoneos.*$/im,
+        explicitExpr = /(.*ARCHS=").*?(".*VALID_ARCHS=").*?(".*)/;
+    var content = fs.readFileSync(buildPath, 'utf-8');
+    var origin = content.match(grossExpr)[0];
+    if(!origin) throw new Error('Can\'t find iphoneos build script line in "' + buildPath + '".');
+
+    var archsStr = archs.join(' '),
+        replacement = origin.replace(explicitExpr, '$1' + archsStr + '$2' + archsStr + '$3');
+    fs.writeFileSync(buildPath, content.replace(origin, replacement), 'utf-8');
+}
